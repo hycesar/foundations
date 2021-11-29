becd222e2c8c0af8c2aad79a743864b19d248cf2
hycesar <hcbp@cin.ufpe.br>
2021-11-28 21:39:00 -0300

    Initial Commit
    Init

---
+++ Foundations/FoundationsApp.swift
@@ -0,0 +1,18 @@
+//
+//  FoundationsApp.swift
+//  Foundations
+//
+//  Created by Jose Roberto on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FoundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            login()
+        }
+    }
+}
